a383a03fab4330749ae7ed9a315221d6dc5103ab
Anna-Robredo-Benages <anna.robredo01@estudiant.upf.edu>
2022-04-20 23:35:03 +0200

    Add Rollaball Project

---
+++ Rollaball/Assets/Scripts/ObstacleMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ObstacleMovement : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other) {
+        if(other.transform.CompareTag("Player")) 
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
